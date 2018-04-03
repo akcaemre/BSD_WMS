a577438c234fb7c51cd848afdbdca16593760c8e
akcaemre <emre_a1@hotmail.com>
2018-04-03 15:43:23 +0200

    initial commit

--- start_mac.sh
+++ start_mac.sh
@@ -10,10 +10,6 @@ osascript -e 'tell app "Terminal"
 end tell'
 
 osascript -e 'tell app "Terminal"
-    do script "mongo --host 127.0.0.1:27017" 
-end tell'
-
-osascript -e 'tell app "Terminal"
     do script "cd <Path-to-AngularJS>>~/Documents/BSD_WMS/AngularJS
 npm run-script start" 
 end tell'
