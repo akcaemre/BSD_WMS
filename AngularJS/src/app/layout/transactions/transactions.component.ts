4e56fda392cc8c3376a42ea146d919e19072caee
akcaemre <emre_a1@gmeil.com>
2018-04-12 08:01:26 -0700

    Transaction site edited

--- AngularJS/src/app/layout/transactions/transactions.component.ts
+++ AngularJS/src/app/layout/transactions/transactions.component.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs/Observable';
 import { Response } from '@angular/http/src/static_response';
 import 'rxjs/add/operator/map'
 import { AppComponent } from '../../app.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
     selector: 'app-transactions',
@@ -13,73 +14,137 @@ import { AppComponent } from '../../app.component';
     styleUrls: ['./transactions.component.scss']
 })
 export class TransactionComponent implements OnInit {    
-    public transactions: Observable<Array<any>>;
     private _transactions : Array<any> = new Array<any>();
 
-    constructor(private router: Router, private http : Http) {
-        this.transactions = this.getTransactions();
-        this.transactions.subscribe(
-            res => this._transactions.push(res)
-        );
-    }
+    constructor(private router: Router, private http : Http) { }
 
     ngOnInit() {
-        //this.getTransactions();
+        this.getTransactions();
     }
 
     getTransactions(){
-        return this.http.get(AppComponent.getLink("read", "&table=Transaktionen"))
-        .map((res : Response) => res.json());
+        var theader : HTMLElement = document.getElementById('myTable').getElementsByTagName('thead')[0];
+        var theaderOut : string = "";
 
-        /*
-        this.i = 1;
-        var tbody : HTMLTableSectionElement = document.getElementById('myTable').getElementsByTagName('tbody')[0];
-        tbody.innerHTML +=  "<fieldset class=\"form-group\">";
-
-		this.http.get(this.readUrl).subscribe(res =>{
+		this.http.get(AppComponent.getLink("read", "&table=Transaktionen")).subscribe(res =>{
             res.json().map(e => {
-
-                tbody.innerHTML += 
-                "<tr class=\" " + (e.Typ == "Auslagerung" ? "table-danger" : "table-success") + "\">"
-                + "<th scope=\"row\">" + this.i + "</th>"
-                + "<td>" + e.Name + "</td>"
-                + "<td>" + e.Produkt + "</td>"
-                + "<td>" + e.Menge + "</td>"
-                + "<td>" + e.Einheit + "</td>"
-                + "<td>" + e.PreisProEinheit + "</td>"
-                + "<td>" + e.Gesamtpreis + "</td>"
-                + "<td>" + e.Währung + "</td>"
-                + "<td>" + e.Datum + "</td>"
-                + "<td>" + '<div class="radio"><input type="radio" name="optionsRadios" id="btnRadio' + this.i + '" value="option' + this.i + '"></div>' +  "</td>"
-              + "</tr>";
-                this.i++;
-            })});
-
-            tbody.innerHTML += "</fieldset>";
-            */
+                this._transactions.push(e);
+            })
+            theaderOut += "<tr><td>Nr</td>";
+            for(var key in this._transactions[0])
+                if(!key.includes("id") && !key.includes("Typ"))
+                    theaderOut += "<td>" + key + "</td>";
+    
+            theaderOut += "<td>#</td>";
+            theaderOut += "</tr>";
+    
+            theader.innerHTML = theaderOut;
+            this.generateTable(this._transactions);
+        });            
     }
 
-    public printTransBySelection() {
-        for(var idx = 1; idx <= this._transactions[0].length; idx++)  {
-            var b : any = document.getElementById("btnRadio" + idx);
-            if(b.checked) {
-                this.printTrans(idx-1); break;
+    generateTable(data : Array<any>) {
+        var tbody : HTMLTableSectionElement = document.getElementById('myTable').getElementsByTagName('tbody')[0];
+        var tbodyOut : string = "";
+        
+        for(var idx = 0; idx < data.length; idx++) {
+            tbodyOut += "<tr class='" 
+            + (data[idx]["Typ"] == "Auslagerung" ? "table-danger" : "table-success")
+            + "'><th scope=\"col\">" + (idx+1) + "</th>";
+
+            for (var key in this._transactions[0]) {
+                if(!key.includes("id") && !key.includes("Typ")) {
+                    if(key != "Bezahlt")
+                        tbodyOut += "<td>" + (data[idx][key] == undefined ? "" : data[idx][key]) + "</td>";
+                    else
+                        tbodyOut += "<td>" 
+                                        + (data[idx][key] == "true" ? "Ja" : "Nein") 
+                                    + "</td>";
+                }
             }
+            tbodyOut += 
+            '<td><div class="radio">'
+                + '<input type="radio" name="optionsRadios" '
+                + 'id="btnRadio' + idx +'" value="option"' + idx +'"/>'
+            + '</div></td>';
+            tbodyOut +=  "</tr>";
+        }
+            
+        tbody.innerHTML = tbodyOut;
+    }
+    
+    public payTransBySelection() {
+        this.payTrans(this.getSelectedRB());
+    }
+
+    private payTrans(row : any) {
+        if(this._transactions[row].Bezahlt == "false") {
+            var json = {"source": {
+                    "Name": this._transactions[row].Name,
+                    "Produkt": this._transactions[row].Produkt,
+                    "Menge": this._transactions[row].Menge,
+                    "Einheit": this._transactions[row].Einheit,
+                    "PreisProEinheit": this._transactions[row].PreisProEinheit,
+                    "Gesamtpreis": this._transactions[row].Gesamtpreis,
+                    "Waehrung": this._transactions[row].Waehrung,
+                    "Datum": this._transactions[row].Datum,
+                    "Bezahlt": this._transactions[row].Bezahlt
+                },"destination":{
+                    "Name": this._transactions[row].Name,
+                    "Produkt": this._transactions[row].Produkt,
+                    "Menge": this._transactions[row].Menge,
+                    "Einheit": this._transactions[row].Einheit,
+                    "PreisProEinheit": this._transactions[row].PreisProEinheit,
+                    "Gesamtpreis": this._transactions[row].Gesamtpreis,
+                    "Waehrung": this._transactions[row].Waehrung,
+                    "Datum": this._transactions[row].Datum,
+                    "Typ": this._transactions[row].Typ,
+                    "Bezahlt": "true",
+                    "BezahltVon": HeaderComponent.getUsername(),
+                    "BezahltAm": new Date()
+                }};
+            console.log(json);
+            this.http.put(
+                AppComponent.getLink("update", "&table=Transaktionen"), json)
+            .map(res => res.json()).subscribe(
+            (data) => {
+                console.log("SUCCESS");
+                console.log(data);
+                this.getTransactions();
+            },
+            (err) => console.log(err + " ErrorCode: " + err.status)
+            );
         }
     }
 
+    public printTransBySelection() {
+        this.printTrans(this.getSelectedRB());
+    }
+
+    private getSelectedRB() {
+        var toReturn = 0;
+        for(var idx = 0; idx <= this._transactions.length; idx++)  {
+            var b : any = document.getElementById("btnRadio" + idx);
+
+            if(b.checked) {
+                toReturn = idx;
+                break;
+            }
+        }
+        return toReturn;
+    }
+
     public printTrans (row) {
-        this._transactions.forEach(function(next) {console.log(next);} );
         var content = this.getContent(
-            this._transactions[0][row].Name,
-            this._transactions[0][row].Produkt,
-            this._transactions[0][row].Menge,
-            this._transactions[0][row].Einheit,
-            this._transactions[0][row].PreisProEinheit,
-            this._transactions[0][row].Gesamtpreis,
-            this._transactions[0][row].Waehrung,
-            this._transactions[0][row].Datum,
-            this._transactions[0][row].Bezahlt
+            this._transactions[row].Name,
+            this._transactions[row].Produkt,
+            this._transactions[row].Menge,
+            this._transactions[row].Einheit,
+            this._transactions[row].PreisProEinheit,
+            this._transactions[row].Gesamtpreis,
+            this._transactions[row].Waehrung,
+            this._transactions[row].Datum,
+            this._transactions[row].Bezahlt
         );
         var win = window.open("", "html", "resizable,scrollbars,status");
 
